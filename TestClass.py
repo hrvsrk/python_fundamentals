12ecb69824c110821c0fdab08e80f46253ed060b
hrvsrk <hr.vsrk@gmail.com>
2020-03-18 23:52:36 +0530

    Add files via upload

---
+++ TestClass.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Dog:
+    """A simple attempt to model a dog"""
+    
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        print("Successfully executed and called out")
+        
+    def sit(self):
+        """Make the dog sit"""
+        print(f" {self.name} is sitting")
+        
+    def rollover(self):
+        """Make the dog rollover"""
+        print(f" {self.name} has rolled over")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
